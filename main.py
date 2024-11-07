75e4d54b8c818f4cd29756a77cfc07fc996eaa5f
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-11-07 09:39:20 -0500

    bijb

---
+++ main.py
@@ -0,0 +1,8 @@
+rows = int(input("How many rows do u want?"))
+
+for row in range(1, rows + 1):
+    for star in range(1, row + 1):
+       print("* ")
+       end=""
+       # example print("stuff", end="")
+    print(end)
